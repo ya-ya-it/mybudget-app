41f6abb6d91fc7bd1967d28e1e303a6cca05dbb9
ya-ya-it <ya-ya-it@yandex.com>
2018-08-04 01:07:24 -0400

    fixed GIT api for heroku. FINALLY!!!

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ passport.use(
   new GitHubStrategy({
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/github/callback'
+      callbackURL: 'https://mybudget-app.herokuapp.com/github/callback'
     },
     function(accessToken, refreshToken, profile, done) {
       User.findOrCreate({
